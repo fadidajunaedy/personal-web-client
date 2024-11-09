7b82853321ba4512d5dfe5923af45185935f2055
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2024-11-10 00:31:27 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://staging-hris.my.id",
+  base: "https://fadidajunaedy.vercel.app",
   plugins: [react()],
 })
